1ac630dc8847f2489ed6ff27da2d127bdd1f7768
Moharu <joaotelo.nh@hotmail.com>
2015-12-29 16:17:12 +0000

    changed selected ban map displaying style

--- public/scripts/csgobans.js
+++ public/scripts/csgobans.js
@@ -1,3 +1,6 @@
+var defaultBorder = '1.5px solid rgb(221, 221, 221)';
+var bannedBorder = '2.5px solid rgb(255, 0, 0)';
+
 var toggleFade = function(map){
     var mapObj = $('#'+map);
     mapObj.css('opacity',mapObj.css('opacity') === '1'? '0.2' : '1');
@@ -10,6 +13,18 @@ var setFade = function(map, state){
 
 var isFaded = function(map){
     return $('#'+map).css('opacity') === '0.2';
+}
+
+var setMyBan = function(map){
+    if(map==='')
+        return '';
+    $('#'+map).parent().css('border', bannedBorder);
+    //$('#'+map).parent().css('background', 'red');
+}
+
+var resetMyBan = function(map){
+    $('#'+map).parent().css('border', defaultBorder);
+    //$('#'+map).parent().css('background', '');
 }
 
 var resetAllFades = function(){
@@ -50,18 +65,21 @@ $(document).ready(function(){
     $("img").click(function(){
         if(this.id === yourBan){
             yourBan = '';
+            resetMyBan(this.id);
             websocket.send(JSON.stringify({type: 'banMap', map: this.id}));
         } else if(isFaded(this.id)){
             // do nothing
             return null;
         } else if(yourBan === ''){
             yourBan = this.id;
+            setMyBan(yourBan);
             websocket.send(JSON.stringify({type: 'banMap', map: this.id}));
         } else {
             websocket.send(JSON.stringify({type: 'banMap', map: yourBan}));
+            resetMyBan(yourBan);
             websocket.send(JSON.stringify({type: 'banMap', map: this.id}));
             yourBan = this.id;
+            setMyBan(yourBan);
         }
-        $("#yourban").text(yourBan);
     });
 });
